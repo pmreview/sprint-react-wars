44de712396bf2f8e37052e376c41c6b664d7a4b9
JaySebagh <jaysebagh@gmail.com>
2019-05-31 14:12:16 -0700

    character js

---
+++ starwars/src/components/character.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './style.css'
+
+const character = props => {
+    return (
+        <div class="div">
+            <h1 class="h1">Name: {props.characters.name}</h1>
+            <p>Height: {props.characters.height}</p>
+            <p>Mass: {props.characters.mass}</p>
+            <p>Hair Color: {props.characters.hair_color}</p>
+            <p>Skin Color: {props.characters.skin_color}</p>
+            <p>Eye Color: {props.characters.eye_color}</p>
+            <p>Birth Year: {props.characters.birth_year}</p>
+            <p>Gender: {props.characters.gender}</p>
+        </div>
+    )
+}
+
+export default character;
